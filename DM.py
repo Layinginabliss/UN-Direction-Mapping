9fe8edcaca390fc6b9786dac00caf0c190ae910a
Thehf <83724924+Layinginabliss@users.noreply.github.com>
2024-04-05 20:10:57 +0530

    Update DM.py

--- DM.py
+++ DM.py
@@ -1,4 +1,5 @@
 import csv
+
 class Point:
     A = set();B = set();g = 0.0
     def __init__(self,A,B,g):self.A = set(A);self.B = set(B);self.g = g
@@ -7,44 +8,83 @@ class Output:
     def __init__(self,a,b):self.a = a;self.b = b
 def J(A,B):C = len(A & B);return C / (len(A) + len(B) - C)
 
-tf = open('Indexed_mnist_train.csv')
-z = set({'0','1','2','3','4','5','6','7','8','9'});P = [Point(set(),z,1)]
+P = [Point(set(),set({'0','1','2','3','4','5','6','7','8','9'}),1)]
 
-testfile = csv.reader(tf)
-correct = 0
-incorrect = 0
-now = -1
+def map(file_name,P,test_only = False):
+    tf = open(file_name)
+    testfile = csv.reader(tf)
+    correct = 0
+    incorrect = 0
+    now = -1
+    error  = []
 
-for line in testfile:
-    now += 1
-    value = line[0]
-    line.pop(0)
-    O = []
-    max = (0,0)
-    error = 0
-    found = False
-    
-    for y in P:
-        for x in y.B:
-            found_2 = False
-            for o in O:
-                if x == o.a:o.b += y.g * J(set(line),y.A) / len(y.B); found_2 = True;break
-            if not found_2 : O.append(Output(x,y.g * J(set(line),y.A) / len(y.B)))
+    for line in testfile:
+        now += 1
+        value = line[0]
+        line.pop(0)
+        O = []
+        max = (0,0)
+        found = False
+        
+        for y in P:
+            for x in y.B:
+                found_2 = False
+                for o in O:
+                    if x == o.a: 
+                        o.b += y.g * J(set(line),y.A) / len(y.B)
+                        found_2 = True
+                        break
+                if not found_2 :
+                    O.append(Output(x,y.g * J(set(line),y.A) / len(y.B))) 
+                
+        for o in O:
+            if o.b > max[1] : max = (o.a,o.b)
             
-    for o in O:
-        if o.b > max[1] : max = (o.a,o.b)
+        if value == max[0] : correct += 1
+        else: incorrect += 1
         
-    if value == max[0] : correct += 1
-    else: incorrect += 1
-    
-    for r in O:        
-        if r.a == value and max[0] == r.a: found = True;break
+        for r in O:        
+            if r.a == value and max[0] == r.a: found = True;break
+            
+        error.append(0)
+            
+        if not found and not test_only:
+            for r in O:
+                if r.a == value and not r.a == max[0]:
+                    error[len(error)-1]  = max[1] - r.b
+                    P.append(Point(set(line),set(value),1))
+                    break
         
-    if not found:
-        for r in O:
-            if r.a == value and not r.a == max[0]:
-                error = max[1] - r.b
-                P.append(Point(set(line),set(value),1))
-                P.append(Point(set(line),z - set(value),-1));break
+        error_type = '\033[91m'
+        if error[len(error)-1] == 0:
+            error_type = '\033[92m'
+            
+        React = f' : {error_type} React { [ (int(r.a),float("{:.4f}".format(r.b))) for r in O] } \033[0m '
+       # React = ''
+        
+        print(f'T : {now} / 60000 : P {len(P)} :'
+            f'Mean Accuracy { "{:.5f}".format(correct / (correct + incorrect) * 100 ) } % : '
+            f'C \033[92m {correct} \033[0m : Ic \033[91m {incorrect} \033[0m :'
+            f' Value {value} : {error_type} Max { max[0] } \033[0m : '
+            f'\033[91m AR : {float("{:.4f}".format(sum(x for x in error)/len(error)))} \033[0m : '
+            f'{error_type} Error { float("{:.4f}".format(error[len(error)-1])) } \033[0m'
+            + React)
     
-    print(f'P {len(P)} : accuracy { "{:.5f}".format(correct / (correct + incorrect) * 100 ) } % : c \033[92m {correct} \033[0m : ic \033[91m {incorrect} \033[0m : value = {value}: max { max[0] } : \033[91m error { float("{:.4f}".format(error)) } \033[0m : React { [ (int(r.a),float("{:.4f}".format(r.b))) for r in O] }')
+    # T - Data we now at
+    # P - Total records
+    # C - Correct ones thus far
+    # Ic - Incorrect ones thus far
+    # Value - Right value 
+    # Max - Record votted higest value
+    # AR - Avarage Error
+    # Error - Rhe distant between true value and incorrect value
+    # React - Rull election
+  
+# Resalts 12 649 total records , accuracy is 90.25 %   
+# map('Indexed_mnist_train.csv',P)
+# map('Indexed_mnist_train.csv',P)
+# map('data/Indexed_mnist_test.csv',P,True)
+
+# This acived 89.95 %  with total 6887 records
+map('data/Indexed_mnist_train.csv',P)
+map('data/Indexed_mnist_test.csv',P,True)
